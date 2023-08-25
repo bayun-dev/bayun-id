5f08a2ffb2b34fba32ae77caf5e0ad4a9534e778
Yaskov <ms.yaskov@gmail.com>
2023-08-25 20:10:04 +0300

    fixed response on PostSingup

--- src/main/java/dev/bayun/id/api/schema/response/PostSignupResponse.java
+++ src/main/java/dev/bayun/id/api/schema/response/PostSignupResponse.java
@@ -1,11 +1,13 @@
 package dev.bayun.id.api.schema.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @Data
+@JsonSerialize
 @AllArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
